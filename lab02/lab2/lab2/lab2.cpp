70714689ad0ebe5d123e9716e59148a21369219d
Даниил Власов <zmachenko@mail.ru>
2026-03-24 20:44:30 +0700

    lab2

---
+++ lab02/lab2/lab2/lab2.cpp
@@ -0,0 +1,580 @@
+﻿#include "framework.h"
+#include "resource.h"
+#define MAX_LOADSTRING 100
+#include <string>
+#include <vector>
+#include <sstream>
+
+// WEAPON (Базовый класс)
+class Weapon {
+protected:
+    std::string name;
+    int durability;
+    int basePower;
+
+public:
+    Weapon(std::string n, int dur, int power)
+        : name(n), durability(dur), basePower(power) {
+    }
+
+    virtual int attack() = 0;
+    virtual std::string getDescription() = 0;
+    virtual ~Weapon() {}
+
+    std::string getName() { return name; }
+    int getDurability() { return durability; }
+};
+
+// SWORD PHYSICAL
+class SwordPhysical : public Weapon {
+private:
+    int sharpness;
+
+public:
+    SwordPhysical(int sharp)
+        : Weapon("Sword", 100, 50), sharpness(sharp) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + sharpness;
+        return totalPower * 2;
+    }
+
+    std::string getDescription() override {
+        return name + " (Physical)";
+    }
+};
+
+// SWORD MAGIC
+class SwordMagic : public Weapon {
+private:
+    int sharpness;
+
+public:
+    SwordMagic(int sharp)
+        : Weapon("Sword", 100, 50), sharpness(sharp) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + sharpness;
+        return totalPower * 3 + 10;
+    }
+
+    std::string getDescription() override {
+        return name + " (Magic)";
+    }
+};
+
+// SWORD FIRE
+class SwordFire : public Weapon {
+private:
+    int sharpness;
+
+public:
+    SwordFire(int sharp)
+        : Weapon("Sword", 100, 50), sharpness(sharp) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + sharpness;
+        return totalPower * 2 + 5;
+    }
+
+    std::string getDescription() override {
+        return name + " (Fire)";
+    }
+
+    void block() {}
+};
+
+// BOW PHYSICAL
+class BowPhysical : public Weapon {
+private:
+    int range;
+
+public:
+    BowPhysical(int rng)
+        : Weapon("Bow", 80, 30), range(rng) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + range;
+        return totalPower * 2;
+    }
+
+    std::string getDescription() override {
+        return name + " (Physical)";
+    }
+};
+
+// BOW MAGIC
+class BowMagic : public Weapon {
+private:
+    int range;
+
+public:
+    BowMagic(int rng)
+        : Weapon("Bow", 80, 30), range(rng) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + range;
+        return totalPower * 3 + 10;
+    }
+
+    std::string getDescription() override {
+        return name + " (Magic)";
+    }
+};
+
+// BOW FIRE
+class BowFire : public Weapon {
+private:
+    int range;
+
+public:
+    BowFire(int rng)
+        : Weapon("Bow", 80, 30), range(rng) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + range;
+        return totalPower * 2 + 5;
+    }
+
+    std::string getDescription() override {
+        return name + " (Fire)";
+    }
+};
+
+// STAFF PHYSICAL
+class StaffPhysical : public Weapon {
+private:
+    int manaCost;
+
+public:
+    StaffPhysical(int mana)
+        : Weapon("Staff", 60, 40), manaCost(mana) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + manaCost;
+        return totalPower * 2;
+    }
+
+    std::string getDescription() override {
+        return name + " (Physical)";
+    }
+};
+
+// STAFF MAGIC
+class StaffMagic : public Weapon {
+private:
+    int manaCost;
+
+public:
+    StaffMagic(int mana)
+        : Weapon("Staff", 60, 40), manaCost(mana) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + manaCost;
+        return totalPower * 3 + 10;
+    }
+
+    std::string getDescription() override {
+        return name + " (Magic)";
+    }
+};
+
+// STAFF FIRE
+class StaffFire : public Weapon {
+private:
+    int manaCost;
+
+public:
+    StaffFire(int mana)
+        : Weapon("Staff", 60, 40), manaCost(mana) {
+    }
+
+    int attack() override {
+        int totalPower = basePower + manaCost;
+        return totalPower * 2 + 5;
+    }
+
+    std::string getDescription() override {
+        return name + " (Fire)";
+    }
+};
+
+// PLAYER
+class Player {
+private:
+    std::vector<Weapon*> inventory;
+
+public:
+    bool equipWeapon(Weapon* weapon) {
+        if (weapon == nullptr) return false;
+        inventory.push_back(weapon);
+        return true;
+    }
+
+    int useWeapon(int index) {
+        if (index >= 0 && index < (int)inventory.size()) {
+            return inventory[index]->attack();
+        }
+        return -1;
+    }
+
+    void clearInventory() {
+        for (auto weapon : inventory) {
+            delete weapon;
+        }
+        inventory.clear();
+    }
+
+    std::vector<Weapon*>& getInventory() { return inventory; }
+
+    size_t getInventorySize() { return inventory.size(); }
+
+    ~Player() {
+        clearInventory();
+    }
+};
+
+// ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ WINDOWS 
+
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
+
+// Игрок
+Player player;
+int selectedWeaponType = 0;
+int selectedDamageType = 0;
+std::wstring attackResult = L"Оружие не создано";
+
+// Элементы управления
+HWND hComboWeaponType;
+HWND hComboDamageType;
+HWND hBtnCreate;
+HWND hBtnAttack;
+HWND hComboInventory;
+HWND hBtnClear;
+HWND hLblResult;
+HWND hLblInfo;
+
+// ОБЪЯВЛЕНИЯ ФУНКЦИЙ
+
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+void CreateWeapon();
+void Attack();
+void ClearInventory();
+void UpdateInventoryCombo();
+void UpdateInfoLabel();
+
+// ТОЧКА ВХОДА
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_LAB2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance(hInstance, nCmdShow))
+        return FALSE;
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB2));
+    MSG msg;
+
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+// РЕГИСТРАЦИЯ КЛАССА ОКНА
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB2));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_LAB2);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+// ИНИЦИАЛИЗАЦИЯ ПРИЛОЖЕНИЯ
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance;
+
+    HWND hWnd = CreateWindowW(szWindowClass, L"RPG Weapon System",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 600, 550, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+// СОЗДАНИЕ ОРУЖИЯ
+
+void CreateWeapon() {
+    Weapon* weapon = nullptr;
+
+    if (selectedWeaponType == 0) {
+        if (selectedDamageType == 0)
+            weapon = new SwordPhysical(10);
+        else if (selectedDamageType == 1)
+            weapon = new SwordMagic(10);
+        else if (selectedDamageType == 2)
+            weapon = new SwordFire(10);
+    }
+    else if (selectedWeaponType == 1) {
+        if (selectedDamageType == 0)
+            weapon = new BowPhysical(15);
+        else if (selectedDamageType == 1)
+            weapon = new BowMagic(15);
+        else if (selectedDamageType == 2)
+            weapon = new BowFire(15);
+    }
+    else if (selectedWeaponType == 2) {
+        if (selectedDamageType == 0)
+            weapon = new StaffPhysical(20);
+        else if (selectedDamageType == 1)
+            weapon = new StaffMagic(20);
+        else if (selectedDamageType == 2)
+            weapon = new StaffFire(20);
+    }
+
+    if (weapon && player.equipWeapon(weapon)) {
+        std::string desc = weapon->getDescription();
+        attackResult = L"✓ Создано: " + std::wstring(desc.begin(), desc.end());
+        SetWindowTextW(hLblResult, attackResult.c_str());
+    }
+
+    UpdateInventoryCombo();
+    UpdateInfoLabel();
+}
+
+// АТАКА
+
+void Attack() {
+    int selectedIndex = (int)SendMessageW(hComboInventory, CB_GETCURSEL, 0, 0);
+
+    if (selectedIndex == CB_ERR || player.getInventorySize() == 0) {
+        attackResult = L"❌ Выберите оружие из инвентаря!";
+        SetWindowTextW(hLblResult, attackResult.c_str());
+        return;
+    }
+
+    int damage = player.useWeapon(selectedIndex);
+    auto& inventory = player.getInventory();
+    std::string desc = inventory[selectedIndex]->getDescription();
+
+    attackResult = L"⚔ " + std::wstring(desc.begin(), desc.end()) +
+        L" наносит " + std::to_wstring(damage) + L" урона!";
+
+    SetWindowTextW(hLblResult, attackResult.c_str());
+}
+
+// ОЧИСТКА ИНВЕНТАРЯ
+
+void ClearInventory() {
+    player.clearInventory();
+
+    SendMessageW(hComboInventory, CB_RESETCONTENT, 0, 0);
+    SendMessageW(hComboInventory, CB_ADDSTRING, 0, (LPARAM)L"Инвентарь пуст");
+    EnableWindow(hComboInventory, FALSE);
+
+    attackResult = L"🗑 Инвентарь очищен!";
+    SetWindowTextW(hLblResult, attackResult.c_str());
+    UpdateInfoLabel();
+}
+
+// ОБНОВЛЕНИЕ COMBOBOX ИНВЕНТАРЯ
+
+void UpdateInventoryCombo() {
+    SendMessageW(hComboInventory, CB_RESETCONTENT, 0, 0);
+
+    auto& inventory = player.getInventory();
+
+    if (inventory.empty()) {
+        SendMessageW(hComboInventory, CB_ADDSTRING, 0, (LPARAM)L"Инвентарь пуст");
+        EnableWindow(hComboInventory, FALSE);
+    }
+    else {
+        EnableWindow(hComboInventory, TRUE);
+        int index = 1;
+        for (auto weapon : inventory) {
+            std::string desc = weapon->getDescription();
+            std::wstring itemText = L"[" + std::to_wstring(index++) + L"] " +
+                std::wstring(desc.begin(), desc.end());
+            SendMessageW(hComboInventory, CB_ADDSTRING, 0, (LPARAM)itemText.c_str());
+        }
+        SendMessageW(hComboInventory, CB_SETCURSEL, inventory.size() - 1, 0);
+    }
+}
+
+// ОБНОВЛЕНИЕ ИНФОРМАЦИИ
+
+void UpdateInfoLabel() {
+    std::wstringstream ss;
+    ss << L"В инвентаре: " << player.getInventorySize() << L" оружие(я)";
+    SetWindowTextW(hLblInfo, ss.str().c_str());
+}
+
+// ОБРАБОТЧИК ОКНА
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        CreateWindowW(L"STATIC", L"СИСТЕМА КОНФИГУРАЦИИ ОРУЖИЯ",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | SS_CENTER,
+            20, 10, 520, 30, hWnd, nullptr, hInst, nullptr);
+
+        CreateWindowW(L"STATIC", L"Тип оружия:",
+            WS_VISIBLE | WS_CHILD, 20, 55, 100, 25, hWnd, nullptr, hInst, nullptr);
+
+        hComboWeaponType = CreateWindowW(L"COMBOBOX", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | CBS_DROPDOWNLIST | WS_VSCROLL,
+            20, 80, 200, 100, hWnd, nullptr, hInst, nullptr);
+        SendMessageW(hComboWeaponType, CB_ADDSTRING, 0, (LPARAM)L"Меч (Sword)");
+        SendMessageW(hComboWeaponType, CB_ADDSTRING, 0, (LPARAM)L"Лук (Bow)");
+        SendMessageW(hComboWeaponType, CB_ADDSTRING, 0, (LPARAM)L"Посох (Staff)");
+        SendMessageW(hComboWeaponType, CB_SETCURSEL, 0, 0);
+
+        CreateWindowW(L"STATIC", L"Тип урона:",
+            WS_VISIBLE | WS_CHILD, 20, 115, 100, 25, hWnd, nullptr, hInst, nullptr);
+
+        hComboDamageType = CreateWindowW(L"COMBOBOX", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | CBS_DROPDOWNLIST | WS_VSCROLL,
+            20, 140, 200, 100, hWnd, nullptr, hInst, nullptr);
+        SendMessageW(hComboDamageType, CB_ADDSTRING, 0, (LPARAM)L"Физический");
+        SendMessageW(hComboDamageType, CB_ADDSTRING, 0, (LPARAM)L"Магический");
+        SendMessageW(hComboDamageType, CB_ADDSTRING, 0, (LPARAM)L"Огненный");
+        SendMessageW(hComboDamageType, CB_SETCURSEL, 0, 0);
+
+        hBtnCreate = CreateWindowW(L"BUTTON", L"Создать оружие",
+            WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            20, 185, 200, 35, hWnd, (HMENU)1001, hInst, nullptr);
+
+        CreateWindowW(L"STATIC", L"Выберите оружие для атаки:",
+            WS_VISIBLE | WS_CHILD, 20, 230, 200, 25, hWnd, nullptr, hInst, nullptr);
+
+        hComboInventory = CreateWindowW(L"COMBOBOX", L"",
+            WS_VISIBLE | WS_CHILD | WS_BORDER | CBS_DROPDOWNLIST | WS_VSCROLL,
+            20, 255, 350, 100, hWnd, nullptr, hInst, nullptr);
+        SendMessageW(hComboInventory, CB_ADDSTRING, 0, (LPARAM)L"Инвентарь пуст");
+        SendMessageW(hComboInventory, CB_SETCURSEL, 0, 0);
+        EnableWindow(hComboInventory, FALSE);
+
+        hBtnAttack = CreateWindowW(L"BUTTON", L"Атаковать!",
+            WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            20, 300, 165, 35, hWnd, (HMENU)1002, hInst, nullptr);
+
+        hBtnClear = CreateWindowW(L"BUTTON", L"Очистить инвентарь",
+            WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            205, 300, 165, 35, hWnd, (HMENU)1003, hInst, nullptr);
+
+        hLblInfo = CreateWindowW(L"STATIC", L"В инвентаре: 0 оружие(я)",
+            WS_VISIBLE | WS_CHILD | SS_CENTER | WS_BORDER,
+            20, 350, 520, 25, hWnd, nullptr, hInst, nullptr);
+
+        hLblResult = CreateWindowW(L"STATIC", attackResult.c_str(),
+            WS_VISIBLE | WS_CHILD | SS_CENTER | WS_BORDER,
+            20, 390, 520, 50, hWnd, nullptr, hInst, nullptr);
+        break;
+
+    case WM_COMMAND:
+        if (HIWORD(wParam) == CBN_SELCHANGE) {
+            if ((HWND)lParam == hComboWeaponType)
+                selectedWeaponType = (int)SendMessageW(hComboWeaponType, CB_GETCURSEL, 0, 0);
+            else if ((HWND)lParam == hComboDamageType)
+                selectedDamageType = (int)SendMessageW(hComboDamageType, CB_GETCURSEL, 0, 0);
+        }
+
+        if (LOWORD(wParam) == 1001)
+            CreateWeapon();
+        if (LOWORD(wParam) == 1002)
+            Attack();
+        if (LOWORD(wParam) == 1003)
+            ClearInventory();
+        if (LOWORD(wParam) == IDM_ABOUT)
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+        if (LOWORD(wParam) == IDM_EXIT)
+            DestroyWindow(hWnd);
+        break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// ОКНО О ПРОГРАММЕ
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
